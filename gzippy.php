578c6d859cc1785e704f267e075f7d8c2a80992d
shamalt <shamalt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-06 11:24:12 +0000

    First version og GZippy
    git-svn-id: https://plugins.svn.wordpress.org/gzippy/trunk@38637 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gzippy.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: GZippy
+Plugin URI: http://shamalt.scft.hu/wordpress/
+Description: GZippy re-enables gzip compression under WordPress 2.5.
+Author: Shamalt
+Version: 1.0
+Author URI: http://shamalt.scft.hu/
+
+Copyright (C) 2008 Shamalt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function gzippy() {
+	ob_start('ob_gzhandler');
+}
+
+add_action('init', 'gzippy');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== GZippy ===
+
+Contributors: shamalt
+Donate link: http://shamalt.scft.hu/wordpress/
+Link: http://shamalt.scft.hu/wordpress/
+Tags: gzip, plugin
+Requires at least: 2.5
+Tested up to: 2.5
+Stable tag: 1.0
+
+GZippy re-enables gzip compression under WordPress 2.5.
+
+== Description ==
+
+GZippy re-enables gzip compression under WordPress 2.5.
+
+== Frequently Asked Questions ==
+
+== Installation ==
+
+1. Upload the directory `gzippy` to `/wp-content/plugins/`.
+1. Activate the plugin under the Plugins menu in WordPress.
+
+== Screenshots ==
